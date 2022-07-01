9cca21656125c085129f4a21cad383ea52a0530d
thepicture <85333076+thepicture@users.noreply.github.com>
2022-07-01 14:35:02 +0500

    style(Login): change text field's type to password

--- src/containers/LoginPage.tsx
+++ src/containers/LoginPage.tsx
@@ -2,11 +2,19 @@ import { Card, Container, Stack } from "@mui/material";
 import { LoginForm } from "../components/LoginForm";
 
 export function LoginPage() {
-    return (
-        <Card style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '3em' }}>
-            <Stack>
-                <LoginForm />
-            </Stack>
-        </Card>
-    );
+  return (
+    <Card
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        padding: "3em",
+      }}
+    >
+      <Stack>
+        <LoginForm />
+      </Stack>
+    </Card>
+  );
 }
